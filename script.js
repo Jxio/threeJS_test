2b53d62bd122478ed38aa111d69eaa023b519ca2
JIAXUXJ <xj.joye@outlook.com>
2019-02-14 15:50:16 -0800

    add comments about camera, scene

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var scene = new THREE.Scene();
+// only one scene in three.js
         
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-        
+// camera determinate which objects display, differ from scene, three.js has many cameras.
+
 var renderer = new THREE.WebGLRenderer();
         
 renderer.setSize(window.innerWidth, window.innerHeight);
